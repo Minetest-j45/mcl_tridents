cd221d88b015876e40483aa7a2d58194ce2fa780
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-01 11:57:22 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,49 @@
+local S = minetest.get_translator("mcl_tridents")
 
+minetest.register_craftitem("mcl_tridents:trident", {
+	description = S("Trident"),
+	inventory_image = "mcl_trident_inv.png",
+	groups = {},
+  on_use = function(itemstack, user, pointed_thing)
+      local obj = minetest.add_entity(vector.new(0, 10, 0), "mobs:sheep", {naked = true})
+      if obj then
+         obj:set_acceleration(vector.new(0, -10, 0))
+      end
+end
+  end
+})
+
+local TRIDENT_ENTITY={
+	physical = true,
+	pointable = false,
+	visual = "mesh",
+	mesh = "mcl_trident.obj",
+	visual_size = {x=-1, y=1},
+	textures = {"mcl_trident.png"},
+	collisionbox = {-0.19, -0.125, -0.19, 0.19, 0.125, 0.19},
+	collide_with_objects = false,
+	_fire_damage_resistant = true,
+
+	_lastpos={},
+	_startpos=nil,
+	_damage=1,	-- Damage on impact
+	_is_critical=false, -- Whether this arrow would deal critical damage
+	_stuck=false,   -- Whether arrow is stuck
+	_stucktimer=nil,-- Amount of time (in seconds) the arrow has been stuck so far
+	_stuckrechecktimer=nil,-- An additional timer for periodically re-checking the stuck status of an arrow
+	_stuckin=nil,	--Position of node in which arow is stuck.
+	_shooter=nil,	-- ObjectRef of player or mob who shot it
+
+	_viscosity=0,   -- Viscosity of node the arrow is currently in
+	_deflection_cooloff=0, -- Cooloff timer after an arrow deflection, to prevent many deflections in quick succession
+}
+
+local spawn_item = function(self, pos)
+	if not minetest.is_creative_enabled("") then
+		local item = minetest.add_item(pos, "mcl_tridents:trident")
+		item:set_velocity({x=0, y=0, z=0})
+		item:set_yaw(self.object:get_yaw())
+	end
+	mcl_burning.extinguish(self.object)
+	self.object:remove()
+end
